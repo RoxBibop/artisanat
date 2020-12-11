76114427d72de408d94eabce871c0a748b62cf05
Rox Bibop <thibaultm@situaction.fr>
2020-12-11 14:35:01 +0100

    result spinner

--- src/views/Results.vue
+++ src/views/Results.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="main">
     <p class="police results m0">Resultats</p>
-    <p class="m0 txt">Grâce à vos choix lors du jeu, nous avons trouvés {{ rl }} métiers de l'artisanat qui peuvent vous correspondre</p>
+    <div class="loading txt" v-if="loading">
+      <p>Nous sommes en train de traiter vos réponses, merci de patienter...</p>
+      <svg class="spinner" width="65px" height="65px" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
+        <circle class="path" fill="none" stroke-width="6" stroke-linecap="round" cx="33" cy="33" r="30"></circle>
+      </svg>
+    </div>
+    <p class="m0 txt" v-if="!loading">Grâce à vos choix lors du jeu, nous avons trouvés {{ rl }} métiers de l'artisanat qui peuvent vous correspondre</p>
     <div class="ctn">
       <div class="flex">
         <div class="loop" v-for="job in responseData" :key="job.name"> 
@@ -33,6 +39,7 @@
           profile_2 : null,
           profile_3 : null        
         },
+        loading : true,
         responseData : [
          
         ],
@@ -94,6 +101,7 @@
           console.log(res);
           this.responseData = res;
           this.rl = res.length;
+          this.loading = false;
           console.log(this.responseData)
         });
 
@@ -216,6 +224,47 @@
   .ctn .flex .loop:nth-child(3) .circle,
   .ctn .flex .loop:nth-child(3) .content a{
     background-color: rgb(81, 12, 192);
+  }
+
+  $offset: 187;
+  $duration: 1.4s;
+
+  .spinner {
+    animation: rotator $duration linear infinite;
+  }
+
+  @keyframes rotator {
+    0% { transform: rotate(0deg); }
+    100% { transform: rotate(270deg); }
+  }
+
+  .path {
+    stroke-dasharray: $offset;
+    stroke-dashoffset: 0;
+    transform-origin: center;
+    animation:
+      dash $duration ease-in-out infinite, 
+      colors ($duration*4) ease-in-out infinite;
+  }
+
+  @keyframes colors {
+    0% { stroke: #4285F4; }
+    25% { stroke: #DE3E35; }
+    50% { stroke: #F7C223; }
+    75% { stroke: #1B9A59; }
+    100% { stroke: #4285F4; }
+  }
+
+  @keyframes dash {
+    0% { stroke-dashoffset: $offset; }
+    50% {
+      stroke-dashoffset: $offset/4;
+      transform:rotate(135deg);
+    }
+    100% {
+      stroke-dashoffset: $offset;
+      transform:rotate(450deg);
+    }
   }
 
   </style>
